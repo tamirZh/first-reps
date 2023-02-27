2d824e30c55dceebb2b2973fe620beccb1f9bb0e
tamir <tamirlan1729@gmail.com>
2023-02-28 00:37:50 +0700

    again

---
+++ homework8/index.js
@@ -0,0 +1,9 @@
+const myName = 'Tamir';
+const MySurname = 'Zhamsaranov';
+const yearOfBirth = 1990;
+const cityOfBirth = 'Ulan-Ude';
+const myHobby = 'social dance';
+const familyStatus = 'married';
+
+let age = 2023 - yearOfBirth;
+console.log(`Hello world! My name is ${myName}, my surname is ${MySurname}.\nI'am ${age} years old. I was born in ${cityOfBirth}. I'am ${familyStatus} and I have son, he is eleven month old. My and my wife's hobbies is ${myHobby}.`)
